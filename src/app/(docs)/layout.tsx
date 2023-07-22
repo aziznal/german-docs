323fe9e6b36c17cecf8ee071eb303133ed7b7eb6
Aziz <aziznal.dev@gmail.com>
2023-07-22 23:25:11 +0300

    setup layout for header

--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -1,24 +1,64 @@
 import { PropsWithChildren } from "react";
+import Link from "next/link";
+import { LucideGithub, Sandwich } from "lucide-react";
+import Image from "next/image";
 
 export default function DocsLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex h-full flex-col">
-      <header className="flex basis-20 bg-sky-500">Header</header>
+      <header className="foo flex basis-[84px] items-center bg-neutral-900 pr-12 text-white">
+        <div className="flex h-full cursor-pointer items-center justify-center px-6 transition hover:bg-neutral-800 lg:w-[length:var(--left-sidebar-width)] lg:px-0">
+          <Sandwich size={36} />
+        </div>
+
+        <div className="flex grow items-center justify-end gap-4">
+          <span className="flex grow justify-center text-center text-5xl lg:flex-grow-0">
+            <Image
+              src="/germany-flag.png"
+              width={100}
+              height={100}
+              alt="German flag"
+            />
+          </span>
+
+          <div className="hidden gap-4 lg:flex">
+            <span className="w-[200px] bg-neutral-700">Search</span>
+
+            <Link
+              href="/"
+              className="font-bold uppercase"
+            >
+              Landing Page
+            </Link>
+          </div>
+
+          <div className="hidden lg:block">
+            <a
+              href="https://github.com/aziznal/german-docs"
+              target="_blank"
+            >
+              <LucideGithub />
+            </a>
+          </div>
+        </div>
+      </header>
 
       <div className="flex grow">
         {/* Left sidebar */}
-        <div className="flex shrink-0 items-center justify-center basis-96 bg-blue-300">
+        <div className="hidden w-[length:var(--left-sidebar-width)] shrink-0 items-center justify-center bg-neutral-100 lg:flex">
           <span>left sidebar</span>
         </div>
 
-        <div className="flex flex-col grow min-h-full">
-          <main className="grow bg-yellow-400">{children}</main>
+        <div className="flex grow flex-col">
+          <main className="grow px-6 py-20 lg:px-8">{children}</main>
 
-          <footer className="bg-red-400 h-[300px]">Footer</footer>
+          <footer className="h-[300px] bg-neutral-900">Footer</footer>
         </div>
 
         {/* Right sidebar */}
-        <div className="shrink-0 basis-96 bg-green-300">right sidebar</div>
+        <div className="hidden basis-48 bg-green-300 lg:flex lg:shrink-0">
+          right sidebar
+        </div>
       </div>
     </div>
   );
--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function LandingPage() {
   return (
-    <>
+    <div className="h-screen w-screen flex justify-center items-center flex-col">
       <h1>Welcome to German Docs</h1>
-    </>
+
+      <Link href="/getting-started">Visit the docs to get started</Link>
+    </div>
   );
 }
