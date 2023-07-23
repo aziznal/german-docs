e9659ce3664f18bfb5910b7b12ea6c01a86fee25
Aziz <aziznal.dev@gmail.com>
2023-07-23 22:33:29 +0300

    improve left-sidebar responsivness

--- src/components/left-sidebar.tsx
+++ src/components/left-sidebar.tsx
@@ -15,7 +15,7 @@ const LeftSidebar = forwardRef<HTMLDivElement, LeftSidebarProps>(
         {...props}
         ref={ref}
         className={cn(
-          `sticky top-[length:var(--header-height)] h-[length:var(--left-sidebar-height)] shrink-0 flex-col overflow-y-auto bg-neutral-100 px-6 pb-10 pt-16 transition-all duration-200 lg:flex`,
+          `fixed top-[length:var(--header-height)] h-[length:var(--left-sidebar-height)] shrink-0 flex-col overflow-y-auto bg-neutral-100 px-6 pb-10 pt-16 transition-all duration-200 lg:sticky lg:flex`,
           isOpen && "w-[length:var(--left-sidebar-width)]",
           !isOpen && "w-0 flex-shrink overflow-x-hidden px-0",
           className,
