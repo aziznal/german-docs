c681d7bc07efcc93a889fd2349ac6944d5d935cf
Aziz <aziznal.dev@gmail.com>
2023-07-27 01:56:25 +0300

    add highlighting to active heading in table of content

--- src/components/table-of-content.tsx
+++ src/components/table-of-content.tsx
@@ -40,23 +40,38 @@ const TableOfContent = forwardRef<HTMLDivElement, TableOfContentProps>(
         {...props}
         ref={ref}
         className={cn(
-          `sticky top-[length:var(--header-height)] hidden h-fit basis-[250px] flex-col lg:flex lg:shrink-0`,
+          `sticky top-[calc(var(--header-height)_+_48px)] hidden h-fit basis-[180px] flex-col gap-3 lg:flex lg:shrink-0`,
           className,
         )}
       >
+        <span className="absolute left-[3px] top-3 z-0 h-[80%] w-[2px] bg-gray-100"></span>
+
         {headings &&
-          headings.map(({ id, text }) => (
-            <a
-              key={id}
-              href={`#${id}`}
-              className={cn(
-                "block px-4 py-2 text-sm text-neutral-700 hover:text-rose-600",
-                id === activeHeadingId && "text-rose-600",
-              )}
-            >
-              {text}
-            </a>
-          ))}
+          headings.map(({ id, text }) => {
+            const isActive = id === activeHeadingId;
+
+            return (
+              <a
+                key={id}
+                href={`#${id}`}
+                className={cn(
+                  "z-10 flex items-center gap-6 text-sm text-neutral-700 hover:text-rose-600",
+                  isActive && "font-bold text-rose-600",
+                )}
+              >
+                <span
+                  className={cn(
+                    "border-3 block h-2 w-2 shrink-0 rounded-full bg-white outline outline-2 -outline-offset-1 outline-gray-100 transition",
+                    isActive && "bg-rose-600 outline-0",
+                  )}
+                ></span>
+
+                <span className="overflow-x-hidden text-ellipsis whitespace-nowrap">
+                  {text}
+                </span>
+              </a>
+            );
+          })}
       </div>
     );
   },
