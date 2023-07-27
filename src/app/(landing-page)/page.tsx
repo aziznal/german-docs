acab692e0c9194fbc45d139108f550bf593e7329
aziznal <aziznal.dev@gmail.com>
2023-07-27 19:36:37 +0300

    redirect landing page to introduction page in docs

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,16 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function LandingPage() {
-  return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <h1>Welcome to German Docs</h1>
-
-      <Link
-        href="/introduction"
-        className="font-bold text-blue-500 underline hover:text-blue-400"
-      >
-        Visit the docs to get started
-      </Link>
-    </div>
-  );
+  return redirect("/introduction");
 }
