dc8b1113dd7f254173d4d86bd84f6573d552566a
Aziz <aziznal.dev@gmail.com>
2023-07-28 23:09:16 +0300

    improve dark mode colors

--- src/components/left-sidebar.tsx
+++ src/components/left-sidebar.tsx
@@ -42,7 +42,7 @@ const LeftSidebar = forwardRef<HTMLDivElement, LeftSidebarProps>(
         ref={ref}
         {...props}
         className={cn(
-          `fixed top-[length:var(--header-height)] h-[length:var(--left-sidebar-height)] shrink-0 flex-col overflow-y-auto bg-neutral-100 px-6 pb-10 pt-16 transition-all duration-200 dark:bg-neutral-800 lg:sticky lg:flex`,
+          `fixed top-[length:var(--header-height)] h-[length:var(--left-sidebar-height)] shrink-0 flex-col overflow-y-auto bg-neutral-100 px-6 pb-10 pt-16 transition-all duration-200 dark:bg-neutral-900 lg:sticky lg:flex`,
           isOpen && "w-[length:var(--left-sidebar-width)]",
           !isOpen && "w-0 flex-shrink overflow-x-hidden px-0",
           className,
