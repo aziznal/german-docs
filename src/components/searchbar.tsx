8f4ea531343d223b82745a9ed15f533046a31465
Aziz <aziznal.dev@gmail.com>
2023-07-30 01:34:37 +0300

    change search popup position to fixed

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -65,7 +65,7 @@ const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(
           open={isSearchOverlayOpen}
           onOpenChange={setIsSearchOverlayOpen}
         >
-          <DialogContent className="absolute top-52 border-neutral-300 bg-neutral-700">
+          <DialogContent className="fixed top-52 border-neutral-300 bg-neutral-700">
             Hello World!
           </DialogContent>
         </Dialog>
