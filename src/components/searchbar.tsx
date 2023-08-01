faae5cd77750f417a82e032c162a07885cd407df
aziznal <aziznal.dev@gmail.com>
2023-08-01 22:57:24 +0300

    create UI for search overlay

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import { HTMLAttributes, forwardRef, useEffect, useState } from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { useGetPlatform } from "@/hooks/platform";
+import { Input } from "@/components/ui/input";
+import { ArrowRight, Hash, Search } from "lucide-react";
 
 export type SearchBarProps = HTMLAttributes<HTMLDivElement> & {};
 
@@ -51,11 +53,7 @@ const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(
           </div>
 
           <div className="pointer-events-none absolute right-0 hidden cursor-pointer items-center gap-2 pr-3 font-mono text-sm text-muted-foreground lg:flex">
-            {isMacos ? (
-              <span className="mb-[2px] text-lg">⌘</span>
-            ) : (
-              <span>Ctrl</span>
-            )}
+            {isMacos ? <span className="text-lg">⌘</span> : <span>Ctrl</span>}
 
             <span>K</span>
           </div>
@@ -65,8 +63,39 @@ const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(
           open={isSearchOverlayOpen}
           onOpenChange={setIsSearchOverlayOpen}
         >
-          <DialogContent className="fixed top-52 border-neutral-300 dark:bg-neutral-700">
-            Hello World!
+          <DialogContent className="border-neutral-300 p-0 pb-6 dark:bg-neutral-700 md:max-w-[700px] [&>*]:mx-6">
+            {/* Search input */}
+            <div className="mt-12 flex items-center rounded border px-4 focus-within:border-rose-500">
+              <Search />
+
+              <Input
+                placeholder="Search for anything..."
+                className="rounded-0 h-16 border-0 bg-transparent outline-0 ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+              />
+            </div>
+
+            {/* Search results */}
+            <div className="flex max-h-[70dvh] flex-col gap-6 overflow-y-auto lg:max-h-[50vh]">
+              <div className="space-y-3">
+                <div className="text-lg font-bold">Der Die Das</div>
+
+                <div className="flex cursor-pointer items-center justify-between rounded bg-neutral-200 p-3 transition duration-75 hover:bg-rose-500 dark:bg-neutral-600">
+                  <div className="flex gap-2">
+                    <Hash /> <span>Nominative (the The in german)</span>
+                  </div>
+
+                  <ArrowRight />
+                </div>
+
+                <div className="flex cursor-pointer items-center justify-between rounded bg-neutral-200 p-3 transition duration-75 hover:bg-rose-500 dark:bg-neutral-600">
+                  <div className="flex gap-2">
+                    <Hash /> <span>Nominative (the The in german)</span>
+                  </div>
+
+                  <ArrowRight />
+                </div>
+              </div>
+            </div>
           </DialogContent>
         </Dialog>
       </>
