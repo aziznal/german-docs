07d18c657cc65eccc7b49321641588ffb5cb88dc
aziznal <aziznal.dev@gmail.com>
2023-08-07 22:49:17 +0300

    fix broken error page styles

--- src/app/(docs)/page-not-found/page.tsx
+++ src/app/(docs)/page-not-found/page.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function PageNotfound() {
   return (
-    <div className="prose">
+    <div className="prose p-16 dark:text-foreground">
       <h1 className="text-red-400">404 Not found</h1>
       <p>{`The page you're looking for does not exist.`}</p>
-      <Link href="/introduction">Back to docs</Link>
+      <Link
+        href="/introduction"
+        className="dark:text-foreground"
+      >
+        Back to docs
+      </Link>
     </div>
   );
 }
