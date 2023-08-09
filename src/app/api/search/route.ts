cd1afa302bb0f349d32704bb8970f00d431307a7
aziznal <aziznal.dev@gmail.com>
2023-08-09 19:43:59 +0300

    create primitive search algorithm

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,10 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const search = request.nextUrl.searchParams.get("search");
+import { search } from "@/lib/search";
 
-  if (!search)
+export async function GET(request: NextRequest) {
+  const searchInput = request.nextUrl.searchParams.get("q");
+
+  if (!searchInput)
     return new NextResponse("No search term provided", { status: 400 });
 
-  return new NextResponse("Heard you loud and clear!", { status: 200 });
+  const result = await search({ query: searchInput });
+
+  if (!result) return new NextResponse("No results found", { status: 404 });
+
+  return new NextResponse(JSON.stringify(result), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 }
--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs/promises";
+import path from "path";
+
 export type SearchResult = {
   pageName: string;
   headings: {
@@ -7,10 +10,65 @@ export type SearchResult = {
   }[];
 };
 
-export function search({ query }: { query: string }): SearchResult[] {
-  // 1. read all json files in the build folder
-  // 2. find all headings that match the query or contain a paragraph that matches the query
-  // 3. return the results
+const MARKDOWN_SEARCH_INDEX_FOLDER_PATH = path.join(
+  process.cwd(),
+  "search-index",
+);
 
-  throw new Error("Not implemented");
+const MARKDOWN_SEARCH_INDEX_FILE_PATH = `${MARKDOWN_SEARCH_INDEX_FOLDER_PATH}/markdown-index.json`;
+
+export async function search({
+  query,
+}: {
+  query: string;
+}): Promise<SearchResult[] | null> {
+  // 1. read build index
+  const searchIndex = JSON.parse(
+    (await fs.readFile(MARKDOWN_SEARCH_INDEX_FILE_PATH)).toString(),
+  ) as typeof import("../../search-index/markdown-index.json");
+
+  // 2. find all headings that match the query or contain a paragraph that matches the query
+  const matches = searchIndex
+    .filter((page) => {
+      const pageMatches = fuzzify(page.pageName).includes(fuzzify(query));
+
+      const headingsMatches = page.headings.some((heading) => {
+        const headingMatches = fuzzify(heading.title).includes(fuzzify(query));
+
+        const paragraphMatches = heading.paragraphs.some((paragraph) =>
+          fuzzify(paragraph).includes(fuzzify(query)),
+        );
+
+        return headingMatches || paragraphMatches;
+      });
+
+      return pageMatches || headingsMatches;
+    })
+    .slice(0, 10)
+    .map((match) => {
+      return {
+        pageName: match.pageName,
+        headings: match.headings.map((heading) => {
+          return {
+            title: heading.title,
+            matchingParagraph:
+              heading.paragraphs.find((paragraph) =>
+                paragraph.includes(fuzzify(query)),
+              ) ?? "",
+            href: heading.href,
+          };
+        }),
+      };
+    });
+
+  // 3. return the results
+  return matches.length > 0 ? matches : null;
+}
+
+function fuzzify(input: string): string {
+  return input
+    .toLowerCase()
+    .replace(/\s+/g, " ")
+    .replace(/[^a-z0-9 ]/g, "")
+    .trim();
 }
